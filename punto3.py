c2d91d01f7b2e116721a12c49428e8208e286a0b
cmolina12 <c.molinap@uniandes.edu.co>
2024-02-29 01:52:48 -0500

    CODIGO 3 FINAL

--- punto3.py
+++ punto3.py
@@ -2,12 +2,12 @@
 import pulp as pl
 import matplotlib.pyplot as plt
 
-# Conjuntos
+    # Conjuntos
 cultivos = ["LB", "TSA", "MRS", "SDA"]  
 nutrientes = ["Agar", "Levadura", "Peptona", "NaCl", "Fosfato"] 
 recursos = ["Fosfato", "Péptidos", "Aminoácidos", "Extracto de Levadura", "Agar"] #
 
-# Parámetros
+    # Parámetros
 minimo = {"LB": {"Agar": 8.4, "Levadura": 1.26, "Peptona": 10.22, "NaCl": 3, "Fosfato": 0},
           "TSA": {"Agar": 4.33, "Levadura": 1.42, "Peptona": 5.3, "NaCl": 1.26, "Fosfato": 0},
           "MRS": {"Agar": 1.2, "Levadura": 0, "Peptona": 0.8, "NaCl": 0.025, "Fosfato": 0.8},
@@ -32,32 +32,32 @@ composicion = {"Fosfato": {"Agar": 0, "Levadura": 0, "Peptona": 0, "NaCl": 0, "F
 
 total_mezcla = 300 #Cantidad total de mezcla
 
-modelo = pl.LpProblem("Minimizar", pl.LpMinimize)
+modelo = pl.LpProblem("Minimizar", pl.LpMinimize) #Se crea el modelo de minimizacion
 
-# Variables
-x = pl.LpVariable.dicts("x", (cultivos, recursos), lowBound=0, cat="Continuous") #Cantidad de recurso r utilizado para el cultivo c
+    # Variables
+x = pl.LpVariable.dicts("x", (cultivos, recursos), lowBound=0, cat="Continuous") #Cantidad de recurso r utilizado para el cultivo c, tambien se establece el limite inferior de 0 (Restriccion de no negatividad) y se establece que es una variable continua (cat="Continuous"
 
-# Función objetivo
+    # Función objetivo
 modelo += pl.lpSum([x[c][r] * costo[r] for c in cultivos for r in recursos]) #Se busca minimizar el costo total que es la suma del costo de cada recurso utilizado
 
-#Restricciones
+    #Restricciones
 
 #Disponibilidad de recursos
 for r in recursos: #Para cada recurso
-    modelo += pl.lpSum([x[c][r] for c in cultivos]) <= disponibilidad[r] #Restriccion de disponibilidad, la cantidad de recurso utilizado debe ser menor o igual a la cantidad disponible
+    modelo += pl.lpSum([x[c][r] for c in cultivos]) <= disponibilidad[r] #Restriccion de disponibilidad, la sumatoria de la cantidad de recurso r utilizado para cada cultivo debe ser menor o igual a la cantidad disponible de ese recurso    
     
 #Produccion total de cada cultivo
 for c in cultivos: #Para cada cultivo
-    modelo += pl.lpSum([x[c][r] for r in recursos]) == 300 #Restriccion de produccion total, se deben producir 300 kg de cada cultivo
+    modelo += pl.lpSum([x[c][r] for r in recursos]) == 300 #Restriccion de produccion total, la sumatoria de la cantidad de cada recurso utilizado para el cultivo c debe ser igual a la cantidad total de mezcla
 
 #Restricciones de nutrientes
-
 for c in cultivos: #Para cada cultivo
     for n in nutrientes: #Para cada nutriente
         total_nutrient = pl.lpSum([(x[c][r] * (composicion[r][n]*0.01)) for r in recursos]) #Cantidad total de nutriente en la mezcla, se multiplica por 0.01 para convertir el entero a porcentaje
         modelo += total_nutrient >= (minimo[c][n]*0.01) * total_mezcla #Restriccion minima, el porcentaje de nutriente en la mezcla debe ser mayor o igual al minimo, se multiplica por 0.01 para convertir el entero a porcentaje
         modelo += total_nutrient <= (maximo[c][n]*0.01) * total_mezcla #Restriccion maxima, el porcentaje de nutriente en la mezcla debe ser menor o igual al maximo, se multiplica por 0.01 para convertir el entero a porcentaje
-#Imprimir
+
+#Resolver el modelo
 modelo.solve()
 
 #Imprimir resultados, indicar la cantidad de cada recurso que se debe utilizar para cada cultivo, ademas de los porcentajes de cada nutriente en la mezcla final
@@ -69,15 +69,15 @@ for c in cultivos:
     total_mix = pl.lpSum([x[c][r].varValue for r in recursos])
     print("")
     for n in nutrientes:
-        total_nutrient = pl.lpSum([(x[c][r].varValue * composicion[r][n]) for r in recursos])
+        total_nutrient = pl.lpSum([(x[c][r].varValue * composicion[r][n]) for r in recursos])   
         print(f"El porcentaje de {n} en la mezcla final es: {round(pl.value(total_nutrient / total_mix),2)}%")
     print("\n")
 
 print(f"El costo total es: ${round(pl.value(modelo.objective),2)}")
 
-#Grafica 
+    #Graficas 
 
-#Se grafica la cantidad de cada recurso que se debe utilizar para cada cultivo
+    #Se grafica la cantidad de cada recurso que se debe utilizar para cada cultivo
 
 #Grafica de el cultivo LB
 heights = [x["LB"][r].varValue for r in recursos] #Se obtiene la cantidad de cada recurso que se debe utilizar para el cultivo LB
@@ -131,7 +131,7 @@ plt.xticks(rotation=35) #Se rota el nombre de los recursos para que no se superp
 plt.ylim(0,max(heights)*1.2) #Se ajusta el espacio para que no se superpongan las etiquetas
 plt.show() #Se muestra la grafica
 
-#Grafica de nutrientes para cada cultivo
+    #Grafica de nutrientes para cada cultivo
 
 #Grafica de nutrientes para el cultivo LB
 
